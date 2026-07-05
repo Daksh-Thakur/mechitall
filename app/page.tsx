e3196858bcd375903d2e3d7bd75411b063d514e5
Daksh Thakur <daksht115@gmail.com>
2026-07-05 09:22:47 +0530

    deploymentsetup

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1> TEST DEPLOYMENT </h1>
+      <h1> TEST DEPLOYMENT RUNNING</h1>
     </div>
+  );
 }
